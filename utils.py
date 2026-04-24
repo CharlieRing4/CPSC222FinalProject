ddd9fe1aa11bd139910cd21df4385b7831067f8e
CharlieRing4 <cring@zagmail.gonzaga.edu>
2026-04-23 21:35:00 -0700

    utils functions

--- utils.py
+++ utils.py
@@ -8,7 +8,8 @@ from statsmodels.stats.proportion import proportions_ztest
 def clean_habits(df:pd.DataFrame):
     df['Pushups'] = df['Pushups'].fillna(0)
     df['Miles Ran'] = df['Miles Ran'].fillna(0)
-    return df
+    ind_df = df.set_index('Date')
+    return ind_df
     
 
 def clean_activities(df:pd.DataFrame):
@@ -17,19 +18,27 @@ def clean_activities(df:pd.DataFrame):
     date_ser = pd.to_datetime(date_df)
     print(date_ser)
 
+def compute_proportions(df:pd.DataFrame):
+    length = len(df)
+    total = df['Flossed'].sum()
+    return length, total
+
 def flossing_hyp_test(df:pd.DataFrame):
     grouped_by_dotw = df.groupby('Day of the Week')
     weekend_df = pd.concat([grouped_by_dotw.get_group('Friday'),grouped_by_dotw.get_group('Saturday')])
     weekday_df = pd.concat([grouped_by_dotw.get_group('Monday'),grouped_by_dotw.get_group('Tuesday'),grouped_by_dotw.get_group('Wednesday'),grouped_by_dotw.get_group('Thursday'),grouped_by_dotw.get_group('Friday')])
    
-    weekday_length = len(weekday_df)
-    weekend_length = len(weekend_df)
-
-    weekend_floss = weekend_df['Flossed'].sum()
-    weekday_floss = weekday_df['Flossed'].sum()
-    print(weekend_floss)
-    print(weekday_floss)
+    weekend_length, weekend_tot = compute_proportions(weekend_df)
+    weekday_length, weekday_tot = compute_proportions(weekday_df)
     
+
+
+    
+
+
+
+def group_by_month(df:pd.DataFrame):
+    pass
 
 
 
@@ -37,11 +46,11 @@ def main():
     running_df = pd.read_csv('activities.csv')
     habits_df = pd.read_csv('habits.csv')
 
-    # print(goals_df.head())
     # clean_activities(running_df)
     clean_habits_df = clean_habits(habits_df)
-    flossing_hyp_test(clean_habits_df)
 
+    flossing_hyp_test(clean_habits_df)
+    print(clean_habits_df.head())
 
     pass
 
