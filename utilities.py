337dc7953ef5090511b6d149cef81ce67820ce6a
CharlieRing4 <cring@zagmail.gonzaga.edu>
2026-04-23 22:00:55 -0700

    go

--- utilities.py
+++ utilities.py
@@ -27,18 +27,26 @@ def flossing_hyp_test(df:pd.DataFrame):
     grouped_by_dotw = df.groupby('Day of the Week')
     weekend_df = pd.concat([grouped_by_dotw.get_group('Friday'),grouped_by_dotw.get_group('Saturday')])
     weekday_df = pd.concat([grouped_by_dotw.get_group('Monday'),grouped_by_dotw.get_group('Tuesday'),grouped_by_dotw.get_group('Wednesday'),grouped_by_dotw.get_group('Thursday'),grouped_by_dotw.get_group('Friday')])
-   
-    weekend_length, weekend_tot = compute_proportions(weekend_df)
-    weekday_length, weekday_tot = compute_proportions(weekday_df)
     
+    weekend_length, weekend_successes = compute_proportions(weekend_df)
+    weekday_length, weekday_successes = compute_proportions(weekday_df)
 
+    successes = [weekend_successes,weekday_successes]
+    nobs = [weekend_length,weekday_length]
 
-    
-
+    tstat, pval = proportions_ztest(successes,nobs)
+    print(f'Test Statistic: {tstat:.3f}')
+    print(f'PValue: {pval:.3f}')
 
 
 def group_by_month(df:pd.DataFrame):
-    pass
+    jan_df = df.iloc[:31]
+    feb_df = df.iloc[32:59]
+    mar_df = df.iloc[60:90]
+    apr_df = df.iloc[91:]
+
+    
+    print(apr_df.tail())
 
 
 
@@ -49,8 +57,8 @@ def main():
     # clean_activities(running_df)
     clean_habits_df = clean_habits(habits_df)
 
-    flossing_hyp_test(clean_habits_df)
-    print(clean_habits_df.head())
+    # flossing_hyp_test(clean_habits_df)
+    group_by_month(clean_habits_df)
 
     pass
 
